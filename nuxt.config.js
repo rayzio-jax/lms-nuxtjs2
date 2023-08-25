6dee5afbdea39fbf4ad2972139740c4d0afb764f
rayhan <rayhananthaakbar@gmail.com>
2023-08-25 17:21:55 +0700

    update: setup tailwindcss and global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // SCSS file in the project
+    '@/assets/css/main.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -50,5 +53,14 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
 }
