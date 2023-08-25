823af00be078b94567c4ddcfeab658977e4e77b4
rayhan <rayhananthaakbar@gmail.com>
2023-08-25 17:20:12 +0700

    update: create basic navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-[#238492] p-4 flex text-3xl rubik">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+    </div>
+</template>
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+<style lang="scss" scoped>
+.rubik {
+  font-family: 'Rubik', sans-serif;
+  font-weight: 700;
+}
+</style>
