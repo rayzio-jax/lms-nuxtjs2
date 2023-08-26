13196406ea368e3f772351a95a3f215cb305a646
rayhan <rayhananthaakbar@gmail.com>
2023-08-26 13:20:33 +0700

    update: base route path

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'rayzio-jax.github.io/lms-nuxtjs2/',
+    baseURL: '/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -61,5 +61,10 @@ export default {
         },
       },
     },
+  },
+
+  // Base router
+  router: {
+    base: '/lms-nuxtjs2/'
   },
 }
