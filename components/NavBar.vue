f7924f1d0ae6c58682fe587b3af9e342fb240fb0
rayhan <rayhananthaakbar@gmail.com>
2023-08-26 23:45:15 +0700

    update: change coloring

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-[#A8EE1E]/[0.5] rubik-font w-full absolute top-0 py-4 flex text-xl gap-x-8 justify-center text-[#227029] md:text-2xl lg:text-3xl dark:bg-[#789146]/[0.5] dark:text-white">
+        class="bg-[#A8EE1E] rubik-font w-full absolute top-0 py-4 flex text-xl gap-x-8 justify-center text-[#227029] md:text-2xl lg:text-3xl dark:bg-[#789146] dark:text-white">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
         <router-link to="/contact">Contact</router-link>
