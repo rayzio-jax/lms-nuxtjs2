7eeb9ac32b5720eabbdf08a253b53d4d6fe7b075
rayhan <rayhananthaakbar@gmail.com>
2023-08-26 12:58:31 +0700

    update: list my contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,16 @@
         <NavBar />
         <section
             class="h-[100vh] flex flex-col justify-center items-center text-2xl md:text-3xl lg:text-4xl dark:text-white">
-            <h1>Contact</h1>
+            <h1>Here I'm dropping my social if you want to reach me:</h1>
+            <ul class="flex gap-x-5">
+                <li class="text-yellow-500 transition-colors duration-300 hover:text-yellow-800"><a
+                        href="https://github.com/rayzio-jax">Github</a></li>
+                <li class="text-yellow-500 transition-colors duration-300 hover:text-yellow-800"><a
+                        href="https://www.linkedin.com/in/rayhanantha-akbar/">LinkedIn</a></li>
+                <li class="text-yellow-500 transition-colors duration-300 hover:text-yellow-800"><a
+                        href="https://www.instagram.com/rayziojax">Instagram</a>
+                </li>
+            </ul>
         </section>
         <FooterBar />
     </main>
