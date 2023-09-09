016d720417939f13e92efafac72caf264295439f
rayhan <rayhananthaakbar@gmail.com>
2023-09-09 17:13:57 +0700

    update: change page content

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,29 @@
+<!-- eslint-disable no-console -->
 <template>
-    <main>
-        <NavBar />
-        <section
-            class="h-[100vh] flex flex-col justify-center items-center px-12 text-2xl md:text-3xl lg:text-4xl dark:text-white">
-            <p class="text-justify">I'm Rayhan a Junior Web Developer as Front End, my dream is become a Fullstack Web
-                Developer.
-                I have been learning about web development as a front end for more than 1 years and now I'm trying to learn
-                back end.
-                For now, I'm still on progress learning about Back-End (API, DB Relation, Setup SQL & NoSQL DB, etc) and
-                such
-                of back end frameworks (ExpressJS, RESTX, Flask Restful, etc).
-                Wish I can accomplish my dream within a short time.
-            </p>
-        </section>
-        <FooterBar />
-    </main>
+  <section
+    class="h-[100vh] flex flex-col justify-center items-center px-12 text-2xl md:text-3xl lg:text-4xl"
+  >
+    <div class="words-wrapper flex flex-col md:flex-row">
+      <p>I'm a</p>
+      <div class="words">
+        <span>Front End Dev</span>
+        <span>Junior Designer</span>
+        <span>Junior Developer</span>
+        <span>LoFi Listener</span>
+        <span>Front End Dev</span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: "AboutPage",
-    head: {
-        title: 'About',
-    },
+  name: 'AboutPage',
+  head: {
+    title: 'About',
+  },
+  mounted() {},
 }
 </script>
+
+<style lang="scss" src="~/assets/style/about.scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,91 +1,28 @@
 <template>
-  <main>
-    <NavBar />
-    <section
-      class="h-[100vh] flex flex-col justify-center items-center text-justify text-2xl md:text-3xl lg:text-4xl dark:text-white">
-      <h1>Hi, welcome to my page, let's do a quick intro</h1><br>
-      <div class="words-wrapper">
-        <p>I'm a</p>
-        <div class="words">
-          <span>Front End Dev</span>
-          <span>Junior Designer</span>
-          <span>Junior Developer</span>
-          <span>LoFi Listener</span>
-          <span>Front End Dev</span>
-        </div>
-      </div>
-    </section>
-    <FooterBar />
-  </main>
+  <section
+    class="h-[100vh] flex flex-col justify-center items-center text-justify text-2xl md:text-3xl lg:text-4xl"
+  >
+    <h1>Hi, welcome to my page, let's do a quick intro</h1>
+    <br />
+  </section>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  head: {
-    title: 'Home',
+  data() {
+    return {
+      msg: 'lorem20',
+    }
   },
+
+  head() {
+    return {
+      title: `Home | ${this.$config.appName}`,
+    }
+  },
+  mounted() {},
 }
 </script>
 
-<style lang="scss" scoped>
-.words-wrapper {
-  box-sizing: content-box;
-  height: 70px;
-  padding: 50px 30px;
-  display: flex;
-
-  p {
-    padding: 10px 0;
-  }
-
-  .words {
-    overflow: hidden;
-
-    span {
-      padding: 10px 0 10px 20px;
-      display: block;
-      height: 100%;
-      color: #0e6ffc;
-      animation: spin_words 6s infinite;
-      font-family: "Poppins", sans-serif;
-      font-size: 45px;
-      font-weight: 500;
-    }
-  }
-}
-
-@keyframes spin_words {
-  10% {
-    transform: translateY(-112%);
-  }
-
-  25% {
-    transform: translateY(-100%);
-  }
-
-  35% {
-    transform: translateY(-212%);
-  }
-
-  50% {
-    transform: translateY(-200%);
-  }
-
-  60% {
-    transform: translateY(-312%);
-  }
-
-  75% {
-    transform: translateY(-300%);
-  }
-
-  85% {
-    transform: translateY(-412%);
-  }
-
-  100% {
-    transform: translateY(-400%);
-  }
-}
-</style>
+<style lang="scss" scoped></style>
