d50967ce7d54f883d68faf6e74551242a9f3c78c
rayhan <rayhananthaakbar@gmail.com>
2023-09-09 17:18:30 +0700

    update: change component name

---
+++ components/PageFooter.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/first-attribute-linebreak -->
+<template>
+  <footer
+    class="bg-[#A8EE1E] bottom-0 w-full p-2 h-[50px] text-xl md:text-2xl dark:bg-[#789146]"
+  >
+    <h3 class="text-green-800 dark:text-green-400">
+      credit:
+      <a
+        href="https://github.com/rayzio-jax"
+        class="text-[#2d8828] hover:text-[#55e06f] transition-colors duration-300 dark:text-white dark:hover:text-yellow-600"
+      >
+        rayhan
+      </a>
+    </h3>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+}
+</script>
